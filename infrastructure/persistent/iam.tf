8dc755dba1641df73f1fe10994bc63b54f9fc762
ElsaDevOps <inatelsa@mail.example.com>
2025-12-24 14:56:38 +0000

    updated IAM role policy for least permissions and added checkov reccs

--- infrastructure/persistent/iam.tf
+++ infrastructure/persistent/iam.tf
@@ -15,8 +15,10 @@ resource "aws_iam_role" "gitrole" {
 
 data "aws_iam_policy_document" "github_actions_trust" {
   statement {
+    sid     = "AssumeRole"
     effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
+
 
     principals {
       type        = "Federated"
@@ -35,10 +37,87 @@ data "aws_iam_policy_document" "github_actions_trust" {
       values   = ["repo:ElsaDevOps/Gatus-ECS:ref:refs/heads/main", "repo:ElsaDevOps/Gatus-ECS:pull_request"]
     }
   }
+
+
 }
 
+data "aws_iam_policy_document" "github_actions_permissions" {
+  statement {
+    sid    = "ECRAuth"
+    effect = "Allow"
+    actions = [
+    "ecr:GetAuthorizationToken"]
+    resources = ["*"]
+
+  }
+
+  statement {
+    sid    = "ECRPush"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+    "ecr:CompleteLayerUpload"]
+    resources = [aws_ecr_repository.my_gatus.arn]
+
+  }
+
+  statement {
+    sid    = "TerraformState"
+    effect = "Allow"
+    actions = [
+
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::terraform-state-gatus-elsa",
+
+
+    ]
+
+  }
+
+  statement {
+    sid    = "S3ReadPersistentState"
+    effect = "Allow"
+    actions = [
+
+      "s3:GetObject"
+    ]
+    resources = [
+      "arn:aws:s3:::terraform-state-gatus-elsa/persistent/*",
+    ]
+
+  }
+
+  statement {
+    sid    = "S3ReadWriteEphemeralState"
+    effect = "Allow"
+    actions = [
+
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::terraform-state-gatus-elsa/ephemeral/*",
+    ]
+
+  }
+}
 
 resource "aws_iam_role_policy_attachment" "github_actions" {
   role       = aws_iam_role.gitrole.name
-  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+  policy_arn = data.aws_iam_policy_document.github_actions.arn
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name   = "github-actions-role"
+  policy = data.aws_iam_policy_document.github_actions_trust.json
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name   = "github-actions-permissions"
+  policy = data.aws_iam_policy_document.github_actions_permissions.json
 }
