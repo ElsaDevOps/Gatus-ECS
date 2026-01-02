52614ad95f024e0d51430fc3ac9381f5d8f8565d
ElsaDevOps <inatelsa@mail.example.com>
2026-01-02 19:42:22 +0000

    removed extra override line from waf

--- infrastructure/ephemeral/waf.tf
+++ infrastructure/ephemeral/waf.tf
@@ -14,9 +14,7 @@ resource "aws_wafv2_web_acl" "main" {
     name     = "rate-limit"
     priority = 1
 
-    override_action {
-      none {}
-    }
+
 
     statement {
       rate_based_statement {
